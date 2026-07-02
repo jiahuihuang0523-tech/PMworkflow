de656f4dff51258a6a574466011d01f89b35b501
jiahuihuang0523-tech <jiahuihuang0523@gmail.com>
2026-07-02 21:28:23 +0930

    Delete src/modules directory

--- src/modules/new-task/index.js
+++
@@ -1,8 +0,0 @@
-export { categories } from './categories.js';
-export { breakLeaseFields, breakLeaseStatusSteps, finalWaterCalculationFields, breakLeaseCalculationFields, breakLeaseEmailTemplates } from './break-lease.js';
-export { transferLeaseFields, transferLeaseFinalFields, transferLeaseStatusSteps, transferLeaseEmailTemplates } from './transfer-lease.js';
-export { leaseRenewalFields, leaseRenewalStatusSteps, leaseRenewalEmailTemplates } from './lease-renewal.js';
-export { moveInFields, moveInStatusSteps, moveInEmailTemplates } from './move-in.js';
-export { outgoingFields, outgoingStatusSteps, outgoingEmailTemplates } from './outgoing.js';
-export { lettingOnlyFields, lettingOnlyStatusSteps, lettingOnlyEmailTemplates } from './letting-only.js';
-export { maintenanceStatusSteps } from './maintenance.js';
